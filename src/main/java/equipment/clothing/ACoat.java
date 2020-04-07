42252b18c9627a84e32427dd123098b6c7d69062
EyalGreen <grineyal@gmail.com>
2020-04-07 19:16:24 +0300

    Added classes for clothing-type equipment

---
+++ src/main/java/equipment/clothing/ACoat.java
@@ -0,0 +1,14 @@
+package equipment.clothing;
+
+import equipment.ClothingEquipment;
+
+public class ACoat extends ClothingEquipment {
+    public ACoat(String id) {
+        super(id);
+    }
+
+    public String toString() {
+        return "A' Coat";
+    }
+
+}
---
+++ src/main/java/equipment/clothing/Hermonit.java
@@ -0,0 +1,13 @@
+package equipment.clothing;
+
+import equipment.ClothingEquipment;
+
+public class Hermonit extends ClothingEquipment {
+    public Hermonit(String id) {
+        super(id);
+    }
+
+    public String toString() {
+        return "Hermonit";
+    }
+}
