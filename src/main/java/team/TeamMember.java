6ddfcfb06ed1f8893c90bc641126e7f817fdbe5f
EyalGreen <grineyal@gmail.com>
2020-04-07 19:17:17 +0300

    Added TeamMember class

---
+++ src/main/java/team/TeamMember.java
@@ -0,0 +1,46 @@
+package team;
+
+import equipment.Equipment;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class TeamMember {
+    private String personalNumber;
+    private HashMap<String, Equipment> inventory;
+
+    public TeamMember(String personalNumber) {
+        this.personalNumber = personalNumber;
+        this.inventory = new HashMap<String, Equipment>();
+    }
+
+    public String getPersonalNumber() {
+        return personalNumber;
+    }
+
+    public void setPersonalNumber(String personalNumber) {
+        this.personalNumber = personalNumber;
+    }
+
+    public HashMap<String, Equipment> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(HashMap<String, Equipment> inventory) {
+        this.inventory = inventory;
+    }
+
+    public void addItem(Equipment item) {
+        inventory.put(item.getId(), item);
+    }
+
+    public Equipment removeItem(String id) {
+        return inventory.remove(id);
+    }
+
+    public void printItems() {
+        for (String id : inventory.keySet()) {
+            System.out.println(inventory.get(id).toString());
+        }
+    }
+}
